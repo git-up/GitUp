7d9889e0d29d2d2cf443aa88489bad33b028817b
Goran Blažič <goranche@me.com>
2015-08-19 08:24:06 +0200

    added a simple command line completion script and code to install.sh to load it from /etc/bashrc

--- Tool/install.sh
+++ Tool/install.sh
@@ -7,4 +7,8 @@ INSTALL_DIR=`dirname "$INSTALL_PATH"`
 mkdir -p "$INSTALL_DIR"
 ln -sf "$TOOL_PATH" "$INSTALL_PATH"
 
+grep -q gitup-completion /etc/bashrc || cat << EOF >> /etc/bashrc
+gitup="${TOOL_PATH}-completion.sh"; [ -f "\${gitup}" ] && source "\${gitup}"
+EOF
+
 printf "OK"
