79ce87d44559ed52fe03e19239e6c9836b8ff160
Goran Blažič <goranche@me.com>
2015-08-19 17:22:39 +0200

    fixed indentation to double space instead of tab

--- Tool/gitup-completion.sh
+++ Tool/gitup-completion.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -e
 function _gitup() {
-	local cur opts
-	COMPREPLY=()
-	cur="${COMP_WORDS[COMP_CWORD]}"
-	opts="help open map commit stash"
-	COMPREPLY=($(compgen -W "$opts" -- $cur))
-	return 0
+  local cur opts
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  opts="help open map commit stash"
+  COMPREPLY=($(compgen -W "$opts" -- $cur))
+  return 0
 }
 complete -F _gitup gitup
