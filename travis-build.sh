a7c85a84c7c99a8060b4e583e7ca5441c7dbac6f
Pierre-Olivier Latour <pol@mac.com>
2015-08-18 17:33:26 -0700

    Build Release in Travis CI

--- travis-build.sh
+++ travis-build.sh
@@ -6,4 +6,4 @@ XCODE_SCHEME="Application"
 xcodebuild test -scheme "$XCODE_SCHEME"
 
 # Build app
-xcodebuild build -scheme "$XCODE_SCHEME" "CODE_SIGN_IDENTITY="
+xcodebuild build -scheme "$XCODE_SCHEME" -configuration "Release" "CODE_SIGN_IDENTITY="
