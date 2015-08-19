6afefeb8c4c88bc0d40b68e5375913d984436baf
Goran Blažič <goranche@me.com>
2015-08-19 17:34:38 +0200

    added some comments to the script

--- Tool/install.sh
+++ Tool/install.sh
@@ -7,7 +7,11 @@ INSTALL_DIR=`dirname "$INSTALL_PATH"`
 mkdir -p "$INSTALL_DIR"
 ln -sf "$TOOL_PATH" "$INSTALL_PATH"
 
+# Check if gitup-completion is present in /etc/bashrc ( grep -q ...),
+# otherwise (grep || ...) add our completion loading script to /etc/bashrc
+# more info on here documents: http://tldp.org/LDP/abs/html/here-docs.html
 grep -q gitup-completion /etc/bashrc || cat << EOF >> /etc/bashrc
+# load gitup-completion.sh if it exists; enables command line completion
 gitup="${TOOL_PATH}-completion.sh"; [ -f "\${gitup}" ] && source "\${gitup}"
 EOF
 
