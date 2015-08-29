1de12643e437545f8f552cc65d3a2ce3eefe8ed4
Pierre-Olivier Latour <pol@mac.com>
2015-08-29 14:53:57 -0700

    Improved build script for GitUpKit
    [ci skip]

--- GitUpKit/build-release.sh
+++ GitUpKit/build-release.sh
@@ -4,25 +4,30 @@ set -ex
 XCODE_OBJROOT="/tmp/GitUpKit-Build"
 XCODE_SYMROOT="/tmp/GitUpKit-Products"
 
+LIPO=$(xcrun -find lipo)
+
 rm -rf "$XCODE_SYMROOT"
 
+# Build for OS X
 rm -rf "$XCODE_OBJROOT"
 xcodebuild build -sdk "macosx" -scheme "GitUpKit (OSX)" -configuration "Release" "OBJROOT=$XCODE_OBJROOT" "SYMROOT=$XCODE_SYMROOT"
 
-rm -rf "GitUpKit-MacOSX"
-mkdir -p "GitUpKit-MacOSX"
-mv "$XCODE_SYMROOT/Release/GitUpKit.framework" "GitUpKit-MacOSX"
-
+# Build for iPhone Simulator
 rm -rf "$XCODE_OBJROOT"
 xcodebuild build -sdk "iphonesimulator" -scheme "GitUpKit (iOS)" -configuration "Release" "OBJROOT=$XCODE_OBJROOT" "SYMROOT=$XCODE_SYMROOT"
 
-rm -rf "GitUpKit-iPhoneSimulator"
-mkdir -p "GitUpKit-iPhoneSimulator"
-mv "$XCODE_SYMROOT/Release-iphonesimulator/GitUpKit.framework" "GitUpKit-iPhoneSimulator"
-
+# Build for iOS
 rm -rf "$XCODE_OBJROOT"
 xcodebuild build -sdk "iphoneos" -scheme "GitUpKit (iOS)" -configuration "Release" "OBJROOT=$XCODE_OBJROOT" "SYMROOT=$XCODE_SYMROOT"
 
-rm -rf "GitUpKit-iPhoneOS"
-mkdir -p "GitUpKit-iPhoneOS"
-mv "$XCODE_SYMROOT/Release-iphoneos/GitUpKit.framework" "GitUpKit-iPhoneOS"
+# Copy for OS X
+rm -rf "GitUpKit-OSX"
+mkdir -p "GitUpKit-OSX"
+mv "$XCODE_SYMROOT/Release/GitUpKit.framework" "GitUpKit-OSX"
+
+# Copy for iOS (universal simulator & device)
+rm -rf "GitUpKit-iOS"
+mkdir -p "GitUpKit-iOS"
+mv "$XCODE_SYMROOT/Release-iphonesimulator/GitUpKit.framework" "GitUpKit-iOS"
+$LIPO -create "GitUpKit-iOS/GitUpKit.framework/GitUpKit" "$XCODE_SYMROOT/Release-iphoneos/GitUpKit.framework/GitUpKit" -output "GitUpKit-iOS/GitUpKit.framework/GitUpKit~"
+mv -f "GitUpKit-iOS/GitUpKit.framework/GitUpKit~" "GitUpKit-iOS/GitUpKit.framework/GitUpKit"
