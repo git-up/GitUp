dee0585d272c02f3358ad426fb3d3e6da57575ac
Pierre-Olivier Latour <pol@mac.com>
2015-09-05 13:33:45 -0700

    Build examples as part of Travis CI

--- travis-build.sh
+++ travis-build.sh
@@ -9,3 +9,16 @@ popd
 pushd "GitUp"
 xctool build -scheme "Application" -configuration "Release" "CODE_SIGN_IDENTITY="
 popd
+
+# Build OS X examples
+pushd "Examples/GitDown"
+xctool build -scheme "GitDown" -sdk "macosx" -configuration "Release"
+popd
+pushd "Examples/GitY"
+xctool build -scheme "GitY" -sdk "macosx" -configuration "Release"
+popd
+
+# Build iOS example
+pushd "Examples/iGit"
+xctool build -scheme "iGit" -sdk "iphonesimulator" -configuration "Release"
+popd
