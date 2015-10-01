3b36929610e2d1181e530ae1eb7935430b21ceb7
Pierre-Olivier Latour <pol@mac.com>
2015-09-30 18:24:37 -0700

    Updated libgit2 fork to 73f1742

--- GitUpKit/Core/GCRemote.m
+++ GitUpKit/Core/GCRemote.m
@@ -175,7 +175,7 @@ cleanup:
   
   git_remote_callbacks callbacks = GIT_REMOTE_CALLBACKS_INIT;
   [self setRemoteCallbacks:&callbacks];
-  int status = git_remote_connect(remote, direction, &callbacks);
+  int status = git_remote_connect(remote, direction, &callbacks, NULL);
   if (status != GIT_OK) {
     LOG_LIBGIT2_ERROR(status);
     if (error) {
