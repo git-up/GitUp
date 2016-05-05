1d9115cead89025a6edb6333fce10dd1c150892f
Lucas Derraugh <lucasderraugh@gmail.com>
2016-05-05 19:27:58 -0400

    Add GitHub Desktop button support
    Addresses #74

--- GitUp/Application/AppDelegate.m
+++ GitUp/Application/AppDelegate.m
@@ -346,6 +346,8 @@
   // Initialize Google Analytics
   [[GARawTracker sharedTracker] startWithTrackingID:@"UA-83409580-1"];
 #endif
+  
+  [[NSAppleEventManager sharedAppleEventManager] setEventHandler:self andSelector:@selector(_getUrl:withReplyEvent:) forEventClass:kInternetEventClass andEventID:kAEGetURL];
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification*)notification {
@@ -420,6 +422,16 @@
   // Enable sudden termination
   [[NSProcessInfo processInfo] enableSuddenTermination];
 #endif
+}
+
+- (void)_getUrl:(NSAppleEventDescriptor*)event withReplyEvent:(NSAppleEventDescriptor*)replyEvent {
+  NSURL* url = [NSURL URLWithString:[event paramDescriptorForKeyword:keyDirectObject].stringValue];
+  BOOL isGitHubMacScheme = [url.scheme rangeOfString:@"github-mac" options:NSCaseInsensitiveSearch].location != NSNotFound;
+  BOOL isOpenRepoHost = [url.host rangeOfString:@"openRepo" options:NSCaseInsensitiveSearch].location != NSNotFound;
+  NSString* path = url.path.length ? [url.path substringFromIndex:1] : nil;
+  if (isGitHubMacScheme && isOpenRepoHost && path) {
+    [self _cloneRepositoryFromURLString:path];
+  }
 }
 
 - (BOOL)applicationShouldOpenUntitledFile:(NSApplication*)sender {
@@ -592,8 +604,8 @@ static CFDataRef _MessagePortCallBack(CFMessagePortRef local, SInt32 msgid, CFDa
   }
 }
 
-- (IBAction)cloneRepository:(id)sender {
-  _cloneURLTextField.stringValue = @"";
+- (void)_cloneRepositoryFromURLString:(NSString*)urlString {
+  _cloneURLTextField.stringValue = urlString;
   _cloneRecursiveButton.state = NSOnState;
   if ([NSApp runModalForWindow:_cloneWindow] && _cloneURLTextField.stringValue.length) {
     NSURL* url = GCURLFromGitURL(_cloneURLTextField.stringValue);
@@ -630,6 +642,10 @@ static CFDataRef _MessagePortCallBack(CFMessagePortRef local, SInt32 msgid, CFDa
       [NSApp presentError:MAKE_ERROR(@"Invalid Git repository URL")];
     }
   }
+}
+
+- (IBAction)cloneRepository:(id)sender {
+  [self _cloneRepositoryFromURLString:@""];
 }
 
 - (IBAction)dimissModal:(id)sender {
