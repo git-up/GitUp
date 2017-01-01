59444e74a7e479b18d3329f774a9904c0e00157b
Pierre-Olivier Latour <pol@mac.com>
2017-01-01 21:00:10 +0900

    Reduce build log size for Travis CI

--- travis-build.sh
+++ travis-build.sh
@@ -7,21 +7,21 @@ popd
 
 # Build GitUp without signing
 pushd "GitUp"
-xcodebuild build -scheme "Application" -configuration "Release" "CODE_SIGN_IDENTITY="
+xcodebuild build -scheme "Application" -configuration "Release" "CODE_SIGN_IDENTITY=" > /dev/null
 popd
 
 # Build OS X examples
 pushd "Examples/GitDown"
-xcodebuild build -scheme "GitDown" -sdk "macosx" -configuration "Release"
+xcodebuild build -scheme "GitDown" -sdk "macosx" -configuration "Release" > /dev/null
 popd
 pushd "Examples/GitDiff"
-xcodebuild build -scheme "GitDiff" -sdk "macosx" -configuration "Release"
+xcodebuild build -scheme "GitDiff" -sdk "macosx" -configuration "Release" > /dev/null
 popd
 pushd "Examples/GitY"
-xcodebuild build -scheme "GitY" -sdk "macosx" -configuration "Release"
+xcodebuild build -scheme "GitY" -sdk "macosx" -configuration "Release" > /dev/null
 popd
 
 # Build iOS example
 pushd "Examples/iGit"
-xcodebuild build -scheme "iGit" -sdk "iphonesimulator" -configuration "Release"
+xcodebuild build -scheme "iGit" -sdk "iphonesimulator" -configuration "Release" > /dev/null
 popd
