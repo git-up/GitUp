72854d212f5130237d43591af02764c034145677
Dmitry Lobanov <gaussblurinc@gmail.com>
2019-09-17 12:29:38 +0300

    application: services provider notes have been added.

--- GitUp/Application/ServicesProvider.m
+++ GitUp/Application/ServicesProvider.m
@@ -36,17 +36,30 @@
 
 #pragma mark - Services Provider
 // the minimum method which consists of 3 parameters.
-// 1. Pasteboard: NSPasteboard
-// 2. UserData: NSString
-// 3. Error: *NSString
-- (void)openInFinderEntry:(NSPasteboard *)pasteboard userData:(NSString *)userData error:(NSString * __autoreleasing *)error {
-  
+// 1. methodName: NSPasteboard
+// 2. userData: NSString
+// 3. error: *NSError
+// methodName ( first part ) will be used in .plist to advertise service.
+
+// NOTE:
+// To debug services functionailty.
+// 1. Be sure that methodName ( openRepository ) is used as instance method in plist.
+// 2. Set correct send types in plist. ( NSURLPboardType for urls. )
+// 3. Open Derived data and put .app into ~/Application directory.
+// 4. Rename app if necessary.
+// 5. Use pbs utility to refresh services.
+// 5.1. /System/Library/CoreServices/pbs -update
+// 5.2. /System/Library/CoreServices/pbs -flush
+// 5.3. /System/Library/CoreServices/pbs -dump_cache
+// 6. Be sure that your .app is appearing in dump_cache output.
+// 7. Check finder contextual menu.
+- (void)openRepository:(NSPasteboard *)pasteboard userData:(NSString *)userData error:(NSError * __autoreleasing *)error {
   if ([self canOpenItem:pasteboard]) {
     NSURL *url = [self items:pasteboard].firstObject;
     [self.appDelegate _openRepositoryWithURL:url withCloneMode:kCloneMode_None windowModeID:NSNotFound];
   }
   else {
-    *error = NSLocalizedString(@"Can't open folder!", @"");
+    // fill error if needed.
   }
 }
 
