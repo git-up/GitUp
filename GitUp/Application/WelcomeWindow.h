d24dc83b719a4f31e693e2804e4430290fc04932
Dmitry Lobanov <gaussblurinc@gmail.com>
2019-10-13 23:23:43 +0300

    application: windows welcome window has been incapsulated.

--- GitUp/Application/WelcomeWindow.h
+++ GitUp/Application/WelcomeWindow.h
@@ -6,11 +6,13 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <GitUpKit/GitUpKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface WelcomeWindow : NSWindow
-
+@property(nonatomic, copy) void(^configureItem)(NSMenuItem *item);
+@property(nonatomic, copy) NSArray <NSURL *>*(^getRecentDocuments)();
 @end
 
 NS_ASSUME_NONNULL_END
