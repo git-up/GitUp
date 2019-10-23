1f5c50dc3d23035af26a9ede45f06a99627f2979
Dmitry Lobanov <gaussblurinc@gmail.com>
2019-10-23 20:18:16 +0300

    application: welcome window controller has been introduced.

--- GitUp/Application/WelcomeWindow.h
+++ GitUp/Application/WelcomeWindow.h
@@ -15,4 +15,25 @@ NS_ASSUME_NONNULL_BEGIN
 @property(nonatomic, copy) NSArray <NSURL *>*(^getRecentDocuments)();
 @end
 
+// Add welcome window controller.
+@interface WelcomeWindowControllerModel : NSObject
+@property (assign, nonatomic, readonly) BOOL shouldShow;
+- (void)setShouldShow;
+- (void)setShouldHide;
+
+@property(nonatomic, copy) BOOL(^getUserDefaultsShouldShow)();
+
+@property(nonatomic, copy) void(^configureItem)(NSMenuItem *item);
+@property(nonatomic, copy) NSArray <NSURL *>*(^getRecentDocuments)();
+
+@property(nonatomic, copy) void(^openTwitter)();
+@property(nonatomic, copy) void(^viewIssues)();
+@end
+
+@interface WelcomeWindowController : NSWindowController
+@property (strong, nonatomic, readonly) WelcomeWindowControllerModel *model;
+- (instancetype)configuredWithModel:(WelcomeWindowControllerModel *)model;
+- (void)handleDocumentCountChanged;
+@end
+
 NS_ASSUME_NONNULL_END
--- GitUp/Application/WelcomeWindow.m
+++ GitUp/Application/WelcomeWindow.m
@@ -97,33 +97,125 @@
 }
 @end
 
-@interface WelcomeWindow ()
+@implementation WelcomeWindow
+
+#pragma mark - Setup
+- (void)setup {
+  self.opaque = NO;
+  self.backgroundColor = [NSColor clearColor];
+  self.movableByWindowBackground = YES;
+}
+
+- (void)awakeFromNib {
+  [self setup];
+}
+
+#pragma mark - Window
+- (BOOL)canBecomeKeyWindow {
+  return YES;
+}
+
+@end
+
+@interface WelcomeWindowControllerModel ()
+@property (assign, nonatomic, readwrite) BOOL shouldShow;
+@end
+
+@implementation WelcomeWindowControllerModel
+#pragma mark - Show
+- (void)setShouldShow {
+  self.shouldShow = YES;
+}
+- (void)setShouldHide {
+  self.shouldShow = NO;
+}
+@end
+
+@interface WelcomeWindowController ()
+@property(nonatomic, weak) IBOutlet NSButton *closeButton;
 @property(nonatomic, weak) IBOutlet NSPopUpButton* recentPopUpButton;
 @property(nonatomic, weak) IBOutlet GILinkButton* twitterButton;
 @property(nonatomic, weak) IBOutlet GILinkButton* forumsButton;
 
-@property (weak, nonatomic, readonly) AppDelegate *appDelegate;
-@property (weak, nonatomic, readwrite) IBOutlet WelcomeWindowView *destinationView;
+@property (weak, nonatomic, readonly) WelcomeWindow *welcomeWindow;
+@property (strong, nonatomic, readwrite) WelcomeWindowControllerModel *model;
 @end
 
-@implementation WelcomeWindow
+@implementation WelcomeWindowController
+- (WelcomeWindow *)welcomeWindow {
+  return (WelcomeWindow *)self.window;
+}
+#pragma mark - Initialization
+- (instancetype)init {
+  return [[super initWithWindowNibName:@"Welcome"] configuredWithModel:[[WelcomeWindowControllerModel alloc] init]];
+}
 
-#pragma mark - Accessors
-- (AppDelegate *)appDelegate {
-  return [AppDelegate sharedDelegate];
+#pragma mark - Setup
+- (void)setupUIElements {
+  self.twitterButton.textAlignment = NSLeftTextAlignment;
+  self.twitterButton.textFont = [NSFont boldSystemFontOfSize:11];
+  self.forumsButton.textAlignment = NSLeftTextAlignment;
+  self.forumsButton.textFont = [NSFont boldSystemFontOfSize:11];
+  
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(willShowPopUpMenu) name:NSPopUpButtonWillPopUpNotification object:self.recentPopUpButton];
+  
+  self.closeButton.action = @selector(closeButtonPressed);
+  self.twitterButton.action = @selector(openTwitter);
+  self.forumsButton.action = @selector(viewIssues);
+  
+  self.closeButton.target = self;
+  self.twitterButton.target = self;
+  self.forumsButton.target = self;
+}
+
+#pragma mark - Window Lifecycle
+- (void)windowDidLoad {
+  [super windowDidLoad];
+  WelcomeWindow *welcomeWindow = (WelcomeWindow *)self.window;
+  welcomeWindow.configureItem = self.model.configureItem;
+  welcomeWindow.getRecentDocuments = self.model.getRecentDocuments;
+  
+  [self setupUIElements];
+}
+
+#pragma mark - Configuration
+- (instancetype)configuredWithModel:(WelcomeWindowControllerModel *)model {
+  self.model = model;
+  return self;
+}
+
+#pragma mark - Reactions
+- (void)handleDocumentCountChanged {
+  BOOL showWelcomeWindow = self.model.getUserDefaultsShouldShow();
+  if (showWelcomeWindow && (self.model.shouldShow) && ![[[NSDocumentController sharedDocumentController] documents] count]) {
+    if (!self.welcomeWindow.visible) {
+      [self.welcomeWindow makeKeyAndOrderFront:nil];
+    }
+  } else {
+    if (self.welcomeWindow.visible) {
+      [self.welcomeWindow orderOut:nil];
+    }
+  }
 }
 
 #pragma mark - Actions
+- (void)closeButtonPressed {
+  [self.model setShouldHide];
+  [self.window orderOut:nil];
+}
+
+#pragma mark - Actions/Recent
 - (void)cleanupRecentEntries {
   NSMenu* menu = self.recentPopUpButton.menu;
   while (menu.numberOfItems > 1) {
     [menu removeItemAtIndex:1];
   }
 }
+
 - (void)willShowPopUpMenu {
   [self cleanupRecentEntries];
   NSMenu* menu = self.recentPopUpButton.menu;
-  NSArray* array = self.getRecentDocuments(); // [[NSDocumentController sharedDocumentController] recentDocumentURLs];
+  NSArray* array = self.model.getRecentDocuments(); // [[NSDocumentController sharedDocumentController] recentDocumentURLs];
   if (array.count) {
     for (NSURL* url in array) {
       NSString* path = url.path;
@@ -138,8 +230,8 @@
       }
       NSMenuItem* item = [[NSMenuItem alloc] initWithTitle:title action:NULL keyEquivalent:@""];
       item.representedObject = url;
-      if (self.configureItem) {
-        self.configureItem(item);
+      if (self.model.configureItem) {
+        self.model.configureItem(item);
       }
       [menu addItem:item];
     }
@@ -150,38 +242,16 @@
   }
 }
 
-#pragma mark - Setup
-- (void)setupButtons {
-  // buttons
-  self.twitterButton.textAlignment = NSLeftTextAlignment;
-  self.twitterButton.textFont = [NSFont boldSystemFontOfSize:11];
-  self.forumsButton.textAlignment = NSLeftTextAlignment;
-  self.forumsButton.textFont = [NSFont boldSystemFontOfSize:11];
-  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(willShowPopUpMenu) name:NSPopUpButtonWillPopUpNotification object:self.recentPopUpButton];
-}
-- (void)setup {
-  self.opaque = NO;
-  self.backgroundColor = [NSColor clearColor];
-  self.movableByWindowBackground = YES;
-  [self setupButtons];
+#pragma mark - Actions/Twitter&Issues
+- (void)openTwitter {
+  if (self.model.openTwitter) {
+    self.model.openTwitter();
+  }
 }
 
-- (void)awakeFromNib {
-  [self setup];
+- (void)viewIssues {
+  if (self.model.viewIssues) {
+    self.model.viewIssues();
+  }
 }
-
-#pragma mark - Actions
-- (BOOL)validateMenuItem:(NSMenuItem*)menuItem {
-  return menuItem.action == @selector(performClose:) ? YES : [super validateMenuItem:menuItem];
-}
-
-- (void)performClose:(id)sender {
-  [self.appDelegate closeWelcomeWindow:sender];
-}
-
-#pragma mark - Window
-- (BOOL)canBecomeKeyWindow {
-  return YES;
-}
-
 @end
