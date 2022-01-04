4a482191b3426e0f74013ab94341df37148973ad
Bastian Andelefski <bastian.andelefski@googlemail.com>
2022-01-04 16:31:51 +0100

    Fix #792 missing image comparison (#795)

--- GitUpKit/Interface/GIImageDiffView.m
+++ GitUpKit/Interface/GIImageDiffView.m
@@ -111,21 +111,17 @@
     if (![[NSFileManager defaultManager] fileExistsAtPath:newPath]) {
       [self.repository exportBlobWithSHA1:_delta.newFile.SHA1 toPath:newPath error:&error];
     }
-  } else if (_delta.canonicalPath && _delta.oldFile.SHA1 == nil) {
+  } else {
     newPath = [self.repository absolutePathForFile:_delta.canonicalPath];
   }
-  if (newPath) {
-    _currentImageSize = [self imageSizeWithoutLoadingFromPath:newPath];
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-      NSImage* limitedSizeImage = [self generateLimitedSizeImageFromPath:newPath];
-      dispatch_async(dispatch_get_main_queue(), ^{
-        _currentImageView.image = limitedSizeImage;
-        [self setNeedsDisplay:true];
-      });
+  _currentImageSize = [self imageSizeWithoutLoadingFromPath:newPath];
+  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+    NSImage* limitedSizeImage = [self generateLimitedSizeImageFromPath:newPath];
+    dispatch_async(dispatch_get_main_queue(), ^{
+      _currentImageView.image = limitedSizeImage;
+      [self setNeedsDisplay:true];
     });
-  } else {
-    _currentImageView.image = nil;
-  }
+  });
 }
 
 - (void)updateOldImage {
