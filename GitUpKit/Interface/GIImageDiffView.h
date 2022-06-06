b883945d3d3ccc448b9d4d881b4caa2a26f6643c
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-06 09:16:54 +0800

    Delete GitUpKit directory

--- GitUpKit/Interface/GIImageDiffView.h
+++
@@ -1,8 +0,0 @@
-#import <AppKit/AppKit.h>
-
-@interface GIImageDiffView : NSView
-@property(nonatomic, strong) GCDiffDelta* delta;
-
-- (id)initWithRepository:(GCLiveRepository*)repository;
-- (CGFloat)desiredHeightForWidth:(CGFloat)width;
-@end
