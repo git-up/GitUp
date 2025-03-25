a9c103590944ce4250e6ae6e982e56b553b4d4b8
Nathan Manceaux-Panot <nathan@lemon.garden>
2025-03-25 12:28:17 +0100

    libgit2: Remove tutorial comments from Package.swift

--- GitUpKit/Third-Party/Package.swift
+++ GitUpKit/Third-Party/Package.swift
@@ -70,7 +70,6 @@ let package = Package(
     name: "SwiftPackage",
     platforms: [.macOS(.v10_13)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "Libgit2Origin",
                  targets: ["Libgit2Origin"]
         ),
@@ -82,12 +81,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(name: "Libgit2Origin",
                 dependencies: [
                     "libssh2", "libssl", "libcrypto", "http-client", "ntlmclient"
